5f28bb1350903a31ce59f1465be418ac0d1bd3a8
horiikengo <kengo0805ooo@gmail.com>
2020-05-15 00:13:55 +0900

    コマンド実行前（モデル追加のムービーページスキャフォールディングのやつ）

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public string Index()
+        //{
+        //    return "This is my default action..." ;
+        //}
+        public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
